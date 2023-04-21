6487d9e991d0e7671f7bf5b8faf291f9674855a7
Aurela Gashi <aurelagashi4@gmail.com>
2023-04-21 02:44:47 +0200

    Database

--- HealthService/Program.cs
+++ HealthService/Program.cs
@@ -1,5 +1,17 @@
+using HealthService.Data;
+using HealthService.Models;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase(databaseName: "myInMemoryDb"));
+
+    //CONFIGURING DEPENDENCY INJECTION
+builder.Services.AddScoped<IUserRepo, UserRepo>();
+
+var app = builder.Build();
+
+PrepDb.PrepPopulation(app);
 // Add services to the container.
 
 builder.Services.AddControllers();
