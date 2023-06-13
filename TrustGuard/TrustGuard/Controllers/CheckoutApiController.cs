b2ebce119ae041da49a9885ec554d9600ecfedb5
ermal666 <ermalkk14@gmail.com>
2023-06-13 23:15:53 +0200

    adding Accident Repository

---
+++ TrustGuard/TrustGuard/Controllers/CheckoutApiController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
+using Session = Stripe.BillingPortal.Session;
+using SessionCreateOptions = Stripe.BillingPortal.SessionCreateOptions;
+using SessionService = Stripe.BillingPortal.SessionService;
+
+namespace TrustGuard.Application.Controllers;
+
+[Route("create-checkout-session")]
+[ApiController]
+public class CheckoutApiController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult Create()
+    {
+        var domain = "http://localhost:4242";
+        var options = new SessionCreateOptions
+        {
+            Offer = new List<SessionLineItemOptions>
+            {
+                new SessionLineItemOptions
+                {
+                    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    Price = "{{PRICE_ID}}",
+                    Quantity = 1,
+                },
+            },
+            Mode = "payment",
+            SuccessUrl = domain + "?success=true",
+            CancelUrl = domain + "?canceled=true",
+        };
+        var service = new SessionService();
+        Session session = service.Create(options);
+
+        Response.Headers.Add("Location", session.Url);
+        return new StatusCodeResult(303);
+    }
+}
+    
+}
