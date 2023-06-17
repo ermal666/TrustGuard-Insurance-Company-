262ad29cb129f41795a552eb06b4e58dbaee2653
bleronn <bs52400@ubt-uni.net>
2023-06-17 03:50:53 +0300

    creating initial payment controller

---
+++ TrustGuard/TrustGuard/Controllers/PaymentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrustGuard.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : Controller
+    {
+        public PaymentController()
+        {
+
+        }
+
+
+
+
+    }
+}
