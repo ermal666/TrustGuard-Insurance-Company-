a8661071e0f43fa5b37d8da46d4ebd168c30b4e2
bleronn <bs52400@ubt-uni.net>
2023-06-18 02:29:04 +0200

    *

---
+++ TrustGuard/TrustGuard.Persistence/IRepositories/IPaymentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using TrustGuard.Domain.Models;
+
+namespace TrustGuard.Persistence.IRepositories
+{
+    public interface IPaymentRepository
+    {
+        void CreatePayment(Payment payment);
+        Task CreatePaymentAsync(Payment payment);
+        void CreateRange(List<Payment> payments);
+        Task CreateRangeAsync(List<Payment> payments);
+        void Delete(Payment payment);
+        void DeleteRange(List<Payment> payments);
+        IQueryable<Payment> GetAllPayments();
+        IQueryable<Payment> GetPaymentById(Expression<Func<Payment, bool>> expression);
+        Task<bool> SaveChangesAsync();
+        void Update(Payment payment);
+        void UpdateRange(List<Payment> payments);
+    }
+}
