353690089f4b74bb1c89fa787081731456ea3800
ermalkomoni <69717769+ermal666@users.noreply.github.com>
2023-06-20 00:02:47 +0200

    additional changes

---
+++ TrustGuard/TrustGuard.Front/Components/AppFooter/index.js
@@ -0,0 +1,14 @@
+import { Typography } from "antd";
+
+function AppFooter() {
+  return (
+    <div className="AppFooter">
+      
+      <Typography.Link href="https://www.google.com" target={"_blank"}>
+        Copyright 2023. All Rights Reserved
+      </Typography.Link>
+      
+    </div>
+  );
+}
+export default AppFooter;
